0b614715f620ea3c666d8a55f4cb4ad0b4d5a4ed
VladimirLinuxMint <v.matsnev@yandex.ru>
2019-04-16 16:56:01 +0300

    add history min-max

--- min_max.py
+++ min_max.py
@@ -8,6 +8,8 @@ flask_app = create_app()
 flask_app.app_context().push()
 
 def min_max(data_search):
+    min_temp = []
+    max_temp = []
     date_now_day = data_search.strftime("%d")
     date_now_month = data_search.strftime("%m")
     #print(date_now_day)
@@ -23,21 +25,26 @@ def min_max(data_search):
         for temp in weather_data_history.query.filter(weather_data_history.data == data_a):
             print(temp.temp_max, temp.temp_min)
 
-            a = temp.temp_max.replace(',', '.')
-            b = temp.temp_min.replace(',', '.')
+            #поверка исключения на наличие ","
+            try:
+                a = temp.temp_max.replace(',', '.')
+            except AttributeError:
+                a = temp.temp_max
+            try:
+                b = temp.temp_min.replace(',', '.')
+            except AttributeError:
+                a = temp.temp_min
             print(a, b)
             # замена , на . и на float
-            if float(a) > max_temp:
-                max_temp = temp.temp_max
-            if float(b) < min_temp:
-                min_temp = temp.temp_min
+            max_temp.append(float(a))
+            min_temp.append(float(b))
         i += 1
-    return min_temp, max_temp
+    print(min(min_temp), max(max_temp))
+    return min(min_temp), max(max_temp)
 
 
-min_temp = 0
-max_temp = 0
+
 data_now = datetime.today()
 
-min_max(data_now)
-print(min_temp, max_temp)
+print(min_max(data_now))
+
